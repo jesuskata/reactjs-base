8b287ba7f41833d35925f67c309c455f158c197f
Jesus Romero <rzarate.alex@gmail.com>
2019-08-26 14:29:59 -0500

    Modified the package.json eslintConfig to have a better use on the ReactJs Applications.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable import/prefer-default-export */
 // Dependencies
 import React from 'react';
 
 export const App = () => (
-  <h1>Project prepared to use Babel and React. Including an ESLint configuration.</h1>
+  <h1>Project prepared to use Babel and React. Including an Airbnb ESLint configuration.</h1>
 );
